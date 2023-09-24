55c8af95c5e8294c641d4dd5b2a5ee7493ed9d8a
Aneena <anuanuzz230@gmail.com>
2023-09-24 12:53:14 +0530

    updated..

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class ChatBotView(TemplateView):
-    template_name="chatbot.html"
+    template_name="course-detail.html"
